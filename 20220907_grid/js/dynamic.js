a166a6c91286e52a90d96dfa3b6bcbd1745b62bd
hajin <w2128@e-mirim.hs.kr>
2022-10-19 12:00:04 +0900

    급식 API 호출 url 출력

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -83,4 +83,21 @@ const initButton = () => {
 
 initButton();
 
+//급식 API , AJAX 급식데이터 가져오자
+const KEY = "17af9ee1281d48e5983bcde5fe4dab01";
+console.log(KEY)
 
+const ATPT_OFCDC_SC_CODE = "B10" //서울특별시교육청
+const SD_SCHUL_CODE = "7010569" //미림여자정보과학고
+let MLSV_YMD = "20221019" //2022.10.19
+let MMEAL_SC_CODE = 2; //중식
+
+let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+url += `?KEY=${KEY}`;
+url += `&Type=json`;
+url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+url += `&MLSV_YMD=${MLSV_YMD}`;
+url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
+
+console.log(url)
