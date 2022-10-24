4b43976b7eb28c2d0b68b7f279c4d797fac4e581
hajin <w2128@e-mirim.hs.kr>
2022-10-24 12:07:16 +0900

    js 마우스 이벤트 처리

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -85,24 +85,45 @@ initButton();
 
 //급식 API , AJAX 급식데이터 가져오자
 //.date-grid-container > .grid-item에 mouseover 이벤트 발생->handler 지정
-//handler에서 year, month, date 정보를 가져와서 url 생성
+
+
+const handler = (event) => {
+    //handler에서 year, month, date 정보를 가져와서 url 생성
+    let date = event.target.innerHTML;
+
+    const KEY = "17af9ee1281d48e5983bcde5fe4dab01";
+
+    const ATPT_OFCDC_SC_CODE = "B10" //서울특별시교육청
+    const SD_SCHUL_CODE = "7010569" //미림여자정보과학고
+    let MLSV_YMD = `${year}${month.toString().padStart(2,"0")}${date.padStart(2,"0")}`;
+
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+
+    console.log(url)
+
+    getMenuByAPI(url)
+}
 //AJAX로 url 호출
+const getMenuByAPI = (url) => {
+    //XMLHttpRequest
+    
+    //요청을 보낼 방식, url, 비동기여부 설정
+    //요청 전송
+    //callback
+    
+}
+
 //응답오면, #breakfast, #lunch, #dinner에 출력
+let dateGridContainerDiv = document.getElementsByClassName('date-grid-container')[0];
+let gridItems = dateGridContainerDiv.getElementsByClassName('grid-item');
 
-const KEY = "17af9ee1281d48e5983bcde5fe4dab01";
-console.log(KEY)
-
-const ATPT_OFCDC_SC_CODE = "B10" //서울특별시교육청
-const SD_SCHUL_CODE = "7010569" //미림여자정보과학고
-let MLSV_YMD = "20221019" //2022.10.19
-let MMEAL_SC_CODE = 2; //중식
-
-let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-url += `?KEY=${KEY}`;
-url += `&Type=json`;
-url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
-url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
-url += `&MLSV_YMD=${MLSV_YMD}`;
-url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
-
-console.log(url)
+for(let gridItem of gridItems){
+    // console.log(gridItem)
+    gridItem.onclick = handler;
+    
+}
