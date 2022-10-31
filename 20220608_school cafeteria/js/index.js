aeaa37edf20f887d13c46522120ea9f66782e7ae
hajin <w2128@e-mirim.hs.kr>
2022-10-31 12:15:53 +0900

    js 급식 api 비동기적으로 호출

--- 20220608_school cafeteria/js/index.js
+++ 20220608_school cafeteria/js/index.js
@@ -6,7 +6,7 @@ const showToday = () => {
     let month = now.getMonth() + 1;
     let date = now.getDate();
     let day = now.getDay(); //요일 0~6 0->일
-    
+
     let namesOfTheDaysOfTheWeek_arr = ["일","월",'화','수','목','금','토'];
     day = namesOfTheDaysOfTheWeek_arr[day];
     let title = `${year}.${month}.${date}.${day}`;
@@ -53,5 +53,37 @@ const addNow = (mainCardId) => {
     //now 클래스 추가
     selectedCard.classList.add('now');
 }
-
 addNow('main-card');
+
+//급식 메뉴 표시하자
+const showTodayMenu = () => {
+    //년, 월, 일
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let day = now.getDay(); //요일 0~6 0->일
+    
+    //급식 api
+    //handler에서 year, month, date 정보를 가져와서 url 생성
+    const KEY = "17af9ee1281d48e5983bcde5fe4dab01";
+
+    const ATPT_OFCDC_SC_CODE = "B10" //서울특별시교육청
+    const SD_SCHUL_CODE = "7010569" //미림여자정보과학고
+    let MLSV_YMD = `${year}${month.toString().padStart(2,"0")}${date.toString().padStart(2,"0")}`;
+
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+    console.log(url)
+
+    //비동기로 호출
+    fetch(url).then((response) => response.json()).then((json) => console.log(json));
+
+    //응답오면 표시
+    
+}
+showTodayMenu();
